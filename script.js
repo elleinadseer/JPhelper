77821e0969dacd9450c869761ca0cbbaa2204fcf
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2025-09-04 03:18:40 +0100

    added connections

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.querySelectorAll(".kana").forEach(el => {
     const dakuten = el.querySelector(".dakuten[data-han]");
     const p = el.querySelector("p[data-han]");
-    
-    if(dakuten) dakuten.textContent = dakuten.dataset.han;
-    if(p) p.textContent = p.dataset.han;
+  
+    if(dakuten) {
+      dakuten.textContent = dakuten.dataset.han; // replace text
+      dakuten.style.color = "lightskyblue";              // change color
+    }
+  
+    if(p) {
+      p.textContent = p.dataset.han;             // replace text
+    }
   });
   
